5dfe30b502734f33316f78d7459231b109aaa27c
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-04-25 15:16:11 -0300

    começo da aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 6){
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
