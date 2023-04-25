cb5c3ecac9b5ab6cf53bf09850450409fdeb2684
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-04-25 15:19:22 -0300

    repetição com WHILE e com DO

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,8 +1,21 @@
 var c = 1
-while (c <= 6){
+do {
     console.log(`passo ${c}`)
-    c++ // c = c + 1
-}
+    c++
+} while (c <= 6)
+
+
+
+
+
+
+
+
+// var c = 1
+// while (c <= 6){
+//     console.log(`passo ${c}`)
+//     c++ // c = c + 1
+// }
 
 
 
